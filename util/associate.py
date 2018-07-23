9af46eb74b857af88525b3b7d9da69479cd233c9
zxhou <zxhou2016@gmial.com>
2018-07-23 16:41:06 +0800

    add a license

--- util/associate.py
+++ util/associate.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-This is a script file to associate images and IMU data.
+ * This is a script file to associate images and IMU data.
+
+ * Copyright 2018 Nanjing University of Science and Technology
+ * Author: Zhixing Hou <zxhou@njust.edu.cn>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
 """
 
 fileImage = "D:/Dataset/myKinect/dataIMU1/filenames.txt"
