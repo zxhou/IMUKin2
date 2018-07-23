7033d1f1fabc9c89d10062c2d227901888044d52
zxhou <zxhou2016@gmial.com>
2018-07-23 16:37:44 +0800

    add a utility.

---
+++ util/associate.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+This is a script file to associate images and IMU data.
+"""
+
+fileImage = "D:/Dataset/myKinect/dataIMU1/filenames.txt"
+fileIMU = "D:/Dataset/myKinect/dataIMU1/imuData.txt"
+fileIMU2 = "D:/Dataset/myKinect/dataIMU1/imuDataRemErr.txt"
+fileAss = "D:/Dataset/myKinect/dataIMU1/association.txt"
+timeStampImage = []
+timeStampIMU = []
+timeImage = []
+timeIMU = []
+
+fl = open(fileImage,"r")
+lines = fl.readlines()
+
+
+
+for line3 in lines:
+#    for li in line3:
+    timeStampImage.append(line3.split()[0])
+    timeImage.append(float(line3.split()[0][6:]))
+fl.close()
+    
+fl = open(fileIMU,"r")
+lineIMU = fl.readlines()
+fl.close()
+
+firstIMU = True
+anglePrev = []
+angleCurr = []
+fl = open(fileIMU2,"w")
+for line3 in lineIMU:
+    if firstIMU:
+        timeStampIMU.append(line3.split()[0])
+        timeIMU.append(float(line3.split()[0][6:]))
+        anglePrev = line3.split()[7:10]
+#        print([anglePrev,"\n"])
+        fl.writelines([" ".join(line3.split()), "\n"])
+        firstIMU = False
+    else:
+        angleCurr = line3.split()[7:10]
+        if abs(float(anglePrev[2]))> 178.5 or abs(float(angleCurr[2]))> 178.5:
+            if abs(float(angleCurr[0]) - float(anglePrev[0])) < 3.0 and abs(float(angleCurr[1]) - float(anglePrev[1])) < 3.0 and abs(abs(float(angleCurr[2])) - abs(float(anglePrev[2]))) < 3.0 :
+                timeStampIMU.append(line3.split()[0])
+                timeIMU.append(float(line3.split()[0][6:]))
+                fl.writelines([" ".join(line3.split()), "\n"])
+                anglePrev = angleCurr
+#                print([line3.split()[0], " ", angleCurr,"\n"])
+#            else:
+#                print([line3.split()[0], " ", angleCurr," ", anglePrev, "\n"])
+        else:            
+            if abs(float(angleCurr[0]) - float(anglePrev[0])) < 3.0 and abs(float(angleCurr[1]) - float(anglePrev[1])) < 3.0 and abs(float(angleCurr[2]) - float(anglePrev[2])) < 3.0 :
+                timeStampIMU.append(line3.split()[0])
+                timeIMU.append(float(line3.split()[0][6:]))
+                fl.writelines([" ".join(line3.split()), "\n"])
+                anglePrev = angleCurr
+#                print([line3.split()[0], " ", angleCurr,"\n"])
+#            else:
+#                print([line3.split()[0], " ", angleCurr," ", anglePrev, "\n"])
+fl.close()
+
+fl = open(fileAss,'w')
+
+indImage = 0
+indIMU = 0
+minDiffTime = 10
+
+for tImage in timeImage:
+    for tIMU in timeIMU:
+        diffTime = abs(tImage - tIMU)
+        if diffTime < minDiffTime:
+            minDiffTime = diffTime
+            indIMU = timeIMU.index(tIMU)
+    fl.writelines([timeStampImage[indImage]," ",timeStampIMU[indIMU],"\n"])
+    print([timeStampImage[indImage]," ",timeStampIMU[indIMU],"\n"])
+    indImage = indImage + 1
+    minDiffTime = 10
+ 
+fl.close()
